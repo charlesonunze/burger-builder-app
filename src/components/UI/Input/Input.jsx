fbeea9cd97ef36178af412bc6b5ef06f752acd55
charlesonunze <charlesonunze2@gmail.com>
2020-02-04 10:46:28 +0100

    Render form dynamically (sort of)

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = props => {
+	let inputElement = null;
+
+	switch (props.elementType) {
+		case 'input':
+			inputElement = (
+				<input
+					className={styles.InputElement}
+					{...props.elementConfig}
+					value={props.value}
+				/>
+			);
+			break;
+
+		case 'textarea':
+			inputElement = (
+				<input
+					className={styles.InputElement}
+					{...props.elementConfig}
+					value={props.value}
+				/>
+			);
+			break;
+
+		case 'select':
+			inputElement = (
+				<select
+					className={styles.InputElement}
+					{...props.elementConfig}
+					value={props.value}
+				>
+					{props.elementConfig.options.map(opt => (
+						<option key={opt.value} value={opt.value}>{opt.displayValue}</option>
+					))}
+				</select>
+			);
+			break;
+
+		default:
+			inputElement = (
+				<input
+					className={styles.InputElement}
+					{...props.elementConfig}
+					value={props.value}
+				/>
+			);
+			break;
+	}
+
+	return (
+		<div className={styles.Input}>
+			<label htmlFor=''>{props.label}</label>
+			{inputElement}
+		</div>
+	);
+};
+
+export default Input;
--- src/containers/Checkout/ContactDetails/ContactDetails.jsx
+++ src/containers/Checkout/ContactDetails/ContactDetails.jsx
@@ -1,18 +1,115 @@
 import React from 'react';
 import styles from './ContactDetails.module.css';
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactDetails extends React.Component {
-	state = {};
+	state = {
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name'
+				},
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your E-Mail'
+				},
+				value: '',
+				validation: {
+					required: true,
+					isEmail: true
+				},
+				valid: false,
+				touched: false
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street'
+				},
+				value: '',
+				validation: {
+					required: true
+				},
+				valid: false,
+				touched: false
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code'
+				},
+				value: '',
+				validation: {
+					required: true,
+					minLength: 5,
+					maxLength: 5,
+					isNumeric: true
+				},
+				valid: false,
+				touched: false
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{ value: 'fastest', displayValue: 'Fastest' },
+						{ value: 'cheapest', displayValue: 'Cheapest' }
+					]
+				},
+				value: 'fastest',
+				validation: {},
+				valid: true
+			}
+		},
+		formIsValid: false
+	};
+
 	render() {
+		const { name, email, street, zipCode, deliveryMethod } = this.state.orderForm;
+
 		return (
 			<div className={styles.ContactData}>
 				<h4>Enter your contact details</h4>
 
-				<input type='text' name='name' placeholder='Your Name' />
-				<input type='text' name='email' placeholder='Your Mail' />
-				<input type='text' name='street' placeholder='Street' />
-				<input type='text' name='postal' placeholder='Postal Code' />
+				<Input
+					elementType={name.elementType}
+					elementConfig={name.elementConfig}
+					value={name.value}
+				/>
+				<Input
+					elementType={email.elementType}
+					elementConfig={email.elementConfig}
+					value={email.value}
+				/>
+				<Input
+					elementType={street.elementType}
+					elementConfig={street.elementConfig}
+					value={street.value}
+				/>
+				<Input
+					elementType={zipCode.elementType}
+					elementConfig={zipCode.elementConfig}
+					value={zipCode.value}
+				/>
+				<Input
+					elementType={deliveryMethod.elementType}
+					elementConfig={deliveryMethod.elementConfig}
+					value={deliveryMethod.value}
+				/>
 
 				<Button btnType='Success' clicked={this.props.continuePurchase}>
 					Complete Order
