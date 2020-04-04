c108d415c37ec1795f2c454c674625b190bf8446
charlesonunze <charlesonunze2@gmail.com>
2020-04-04 12:07:44 +0100

    Change spinner

--- src/components/UI/Spinner/Spinner.jsx
+++ src/components/UI/Spinner/Spinner.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import styles from './Spinner.module.css';
 
 const Spinner = props => {
-	return <div className={styles.Spinner}>Loading...</div>;
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<div className={styles.Spinner}>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	);
 };
 
 export default Spinner;
